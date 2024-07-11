8b06eb790339e8efc2c433208741096eefd6379f
wesleyvitor11000 <wesleyvitor37417@gmail.com>
2024-07-11 12:57:21 -0300

    feat: exception handling in user login and creation. #3

--- src/application/users/managers.py
+++ src/application/users/managers.py
@@ -1,23 +1,33 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from users.models import CustomUser
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError(_("The Email must be set"))
+        
         email = self.normalize_email(email)
+        
+        if CustomUser.objects.filter(email=email).exists():
+            raise ValidationError(_("A user with this email already exists"))
+        
         user = CustomUser(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
     def load_user(self, email, password, **extra_fields):
-        users = CustomUser.objects.filter(email=email)
-        if not users or len(users) <= 0:
-            print("Usuário inexistente.")  # TODO raise exception
-            return
+        try:
+            user = CustomUser.objects.get(email=email)
+            
+            if not user.check_password(password):
+                raise ValueError(_("Invalid email or password"))
+            
+        except ObjectDoesNotExist:
+            raise ValueError(_("Invalid email or password"))
 
-        user = users[0]
-        print(user.check_password(password))
+        return user
+    
