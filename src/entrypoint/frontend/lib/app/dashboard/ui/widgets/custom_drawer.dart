32228b8dfe5c2068b0176b2c93a326ee46075dd4
jhoisz <jvrda@ufpi.edu.br>
2024-07-12 22:52:16 -0300

    feat: dashboard components

---
+++ src/entrypoint/frontend/lib/app/dashboard/ui/widgets/custom_drawer.dart
@@ -0,0 +1,79 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:frontend/core/theme/mh_colors.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            color: MHColors.blue,
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  CachedNetworkImage(
+                    imageUrl: '',
+                    height: 50,
+                    width: 50,
+                    placeholder: (context, url) {
+                      return const SizedBox(
+                        height: 20,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    },
+                    errorWidget: (context, url, error) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          color: MHColors.white,
+                          shape: BoxShape.circle,
+                        ),
+                        child: const Center(
+                          child: Icon(
+                            Icons.person,
+                            color: MHColors.blue,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(width: 10),
+                  const Text(
+                    // TODO: Trocar a string 'usuario' pelo nome do usuário
+                    'Usuário',
+                    style: TextStyle(fontSize: 16, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Configurações'),
+            onTap: () {
+              // TODO: Handle Configurações action
+            },
+          ),
+          const Spacer(),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            title: const Text('Sair'),
+            onTap: () {
+              // TODO: Handle Sair action
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
