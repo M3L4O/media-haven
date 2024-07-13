a7afb71af7e78fd7809ee1c4edc38c7fa9e2d091
m3l4o <joaovictor.slv@ufpi.edu.br>
2024-07-12 22:30:38 -0300

    feat: create unit tests from managers and models #6

--- src/application/users/tests.py
+++ src/application/users/tests.py
@@ -1,20 +1,54 @@
-from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from users.managers import CustomUserManager
+from users.models import CustomUser
 
 
 class UsersManagersTests(TestCase):
+    def setUp(self):
+        self.username = "joao"
+        self.email = "joaovictor@gmail.com"
+        self.password = "123456"
+
+        self.user: CustomUser = CustomUserManager.create_user(
+            email=self.email, username=self.username, password=self.password
+        )
 
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email="normal@user.com", password="foo")
-        self.assertEqual(user.email, "normal@user.com")
-        try:
-            self.assertIsNone(user.username)
-        except AttributeError:
-            pass
-        with self.assertRaises(TypeError):
-            User.objects.create_user()
-        with self.assertRaises(TypeError):
-            User.objects.create_user(email="")
-        with self.assertRaises(ValueError):
-            User.objects.create_user(email="", password="foo")
+        self.assertEqual(self.user.username, self.username)
+        self.assertEqual(self.user.email, self.email)
+        self.assertNotEqual(self.user.password, self.password)
+
+        self.assertRaises(
+            ValidationError,
+            CustomUserManager.create_user,
+            email=self.email,
+            username=self.username,
+            password=self.password,
+        )
+
+    def test_login(self):
+        email = "joaovictor@gmail.com"
+        password = "123456"
+
+        user: CustomUser = CustomUserManager.load_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertNotEqual(user.password, password)
+
+        wrong_email = "joaovictor2024@gmail.com"
+        wrong_password = "123abc"
+
+        self.assertRaises(
+            ValueError,
+            CustomUserManager.load_user,
+            email=wrong_email,
+            password=password,
+        )
+        
+        self.assertRaises(
+            ValueError,
+            CustomUserManager.load_user,
+            email=email,
+            password=wrong_password,
+        )
