d9d21b29a0dfd783ff1a93d2ba1d31a644fd56d7
jhoisz <jvrda@ufpi.edu.br>
2024-07-15 09:44:35 -0300

    feat: scrollable login screen

--- src/entrypoint/frontend/lib/app/login/ui/login_page.dart
+++ src/entrypoint/frontend/lib/app/login/ui/login_page.dart
@@ -45,136 +45,137 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverFillRemaining(
-            child: Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    'assets/images/login_bg.jpg',
-                  ),
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/images/login_bg.jpg',
                 ),
+                fit: BoxFit.cover,
               ),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MultiBlocListener(
-                      listeners: [
-                        BlocListener<ILoginBloc, LoginState>(
+            ),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MultiBlocListener(
+                    listeners: [
+                      BlocListener<ILoginBloc, LoginState>(
+                        bloc: loginBloc,
+                        listener: (context, state) {
+                          if (state is LoginFailure) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(state.message),
+                              ),
+                            );
+                          } else if (state is LoginSuccess) {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              '/dashboard',
+                            );
+                          }
+                        },
+                      ),
+                      BlocListener<IRegisterBloc, RegisterState>(
+                        bloc: registerBloc,
+                        listener: (context, state) {
+                          if (state is RegisterFailure) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(state.message),
+                              ),
+                            );
+                          } else if (state is RegisterSuccess) {
+                            Navigator.pushReplacementNamed(
+                              context,
+                              '/dashboard',
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                    child: BlocBuilder(
+                      bloc: registerBloc,
+                      builder: (context, state) {
+                        if (state is RegisterLoading) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+
+                        return BlocBuilder<ILoginBloc, LoginState>(
                           bloc: loginBloc,
-                          listener: (context, state) {
-                            if (state is LoginFailure) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(state.message),
-                                ),
-                              );
-                            } else if (state is LoginSuccess) {
-                              Navigator.pushReplacementNamed(
-                                context,
-                                '/dashboard',
-                              );
-                            }
-                          },
-                        ),
-                        BlocListener<IRegisterBloc, RegisterState>(
-                          bloc: registerBloc,
-                          listener: (context, state) {
-                            if (state is RegisterFailure) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                  content: Text(state.message),
-                                ),
-                              );
-                            } else if (state is RegisterSuccess) {
-                              Navigator.pushReplacementNamed(
-                                context,
-                                '/dashboard',
-                              );
-                            }
-                          },
-                        ),
-                      ],
-                      child: BlocBuilder(
-                          bloc: registerBloc,
                           builder: (context, state) {
-                            if (state is RegisterLoading) {
+                            if (state is LoginLoading) {
                               return const Center(
                                 child: CircularProgressIndicator(),
                               );
                             }
 
-                            return BlocBuilder<ILoginBloc, LoginState>(
-                              bloc: loginBloc,
-                              builder: (context, state) {
-                                if (state is LoginLoading) {
-                                  return const Center(
-                                    child: CircularProgressIndicator(),
-                                  );
-                                }
-
-                                return Form(
-                                  key: formKey,
-                                  child: FormOptions(
-                                    loginMode: loginMode,
-                                    emailController: _emailController,
-                                    passwordController: _passwordController,
-                                    nameController: _nameController,
-                                    confirmPasswordController:
-                                        _confirmPasswordController,
-                                    onChangeLogin: () {
-                                      setState(() {
-                                        loginMode = true;
-                                      });
-                                      _clearForm();
-                                    },
-                                    onChangeRegister: () {
-                                      setState(() {
-                                        loginMode = false;
-                                      });
-                                      _clearForm();
-                                    },
-                                    onTapLogin: () async {
-                                      final isValidate =
-                                          formKey.currentState!.validate();
-                                      if (isValidate) {
-                                        await loginBloc.login(
-                                          email: _emailController.text,
-                                          password: _passwordController.text,
-                                        );
-                                        _clearForm();
-                                      }
-                                    },
-                                    onTapRegister: () async {
-                                      final isValidate =
-                                          formKey.currentState!.validate();
-                                      if (isValidate) {
-                                        await registerBloc.register(
-                                          name: _nameController.text,
-                                          email: _emailController.text,
-                                          password: _passwordController.text,
-                                        );
-                                        _clearForm();
-                                      }
-                                    },
-                                  ),
-                                );
-                              },
+                            return Form(
+                              key: formKey,
+                              child: FormOptions(
+                                loginMode: loginMode,
+                                emailController: _emailController,
+                                passwordController: _passwordController,
+                                nameController: _nameController,
+                                confirmPasswordController:
+                                    _confirmPasswordController,
+                                onChangeLogin: () {
+                                  setState(() {
+                                    loginMode = true;
+                                  });
+                                  _clearForm();
+                                },
+                                onChangeRegister: () {
+                                  setState(() {
+                                    loginMode = false;
+                                  });
+                                  _clearForm();
+                                },
+                                onTapLogin: () async {
+                                  final isValidate =
+                                      formKey.currentState!.validate();
+                                  if (isValidate) {
+                                    await loginBloc.login(
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    );
+                                    _clearForm();
+                                  }
+                                },
+                                onTapRegister: () async {
+                                  final isValidate =
+                                      formKey.currentState!.validate();
+                                  if (isValidate) {
+                                    await registerBloc.register(
+                                      name: _nameController.text,
+                                      email: _emailController.text,
+                                      password: _passwordController.text,
+                                    );
+                                    _clearForm();
+                                  }
+                                },
+                              ),
                             );
-                          }),
+                          },
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
